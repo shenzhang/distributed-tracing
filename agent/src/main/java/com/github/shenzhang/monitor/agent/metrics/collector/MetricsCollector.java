0de2c9465aaa1ffad9df9392d3879b47dd45f3b4
Zhang Shen <shenzhang722@gmail.com>
2017-05-24 23:50:15 +0800

    [Shen] - add some java doc to MetricsCollector to describe how to extend and customize new metrics collection in API

--- agent/src/main/java/com/github/shenzhang/monitor/agent/metrics/collector/MetricsCollector.java
+++ agent/src/main/java/com/github/shenzhang/monitor/agent/metrics/collector/MetricsCollector.java
@@ -3,6 +3,10 @@ package com.github.shenzhang.monitor.agent.metrics.collector;
 import com.github.shenzhang.monitor.agent.domain.Metrics;
 
 /**
+ * If API wants to extend more collector, just need to create a new collector which implements MetricsCollector
+ * and add a instance to spring context. This new collector will be injected to the metrics collection process
+ * automatically. <br/>
+ *
  * User: Zhang Shen
  * Date: 5/17/17
  * Time: 2:56 PM.
